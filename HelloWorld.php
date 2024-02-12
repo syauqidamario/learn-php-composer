420a44707d8a718071af763fd7a135d6394c4bb4
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-02-12 11:42:27 +0700

    First commit. Learning PHP composer from scratch.

---
+++ HelloWorld.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+echo "Hello World" . PHP_EOL;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'syauqidd/learn-php-composer',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'syauqidd/learn-php-composer' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
